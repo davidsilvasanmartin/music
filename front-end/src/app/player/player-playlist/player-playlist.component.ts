3d9706e46e5a96034dbe33c9653ca4ed83ae7025
David Silva <d.silvas@outlook.com>
2025-03-12 17:05:47 +0000

    Improve player

--- front-end/src/app/player/player-playlist/player-playlist.component.ts
+++ front-end/src/app/player/player-playlist/player-playlist.component.ts
@@ -14,7 +14,7 @@ import type { Song } from '../../songs/song';
       >
         <div class="flex h-full flex-col flex-nowrap">
           <div
-            class="sticky top-0 z-10 border-b border-slate-200 bg-white px-4 py-2"
+            class="sticky top-0 z-10 border-b border-slate-200 bg-slate-100 px-4 py-2"
           >
             <div class="flex flex-row flex-nowrap items-center">
               <span class="flex-grow text-base font-bold text-slate-800"
