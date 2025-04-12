becb2509d2f919df9a265911b6152252f57d7b41
David Silva <d.silvas@outlook.com>
2025-04-12 20:48:29 +0100

    Ignore a few properties in AlbumMapper so it does not crash

--- back-end/src/main/java/dev/davidsilva/music/album/AlbumMapper.java
+++ back-end/src/main/java/dev/davidsilva/music/album/AlbumMapper.java
@@ -10,14 +10,22 @@ import java.util.List;
 
 @Mapper(componentModel = "spring", uses = {ArtistMapper.class})
 public interface AlbumMapper {
+    // TODO artist eventually needs to become an object; need to update Album, AlbumDto
     @Mapping(target = "albumArtist", source = "artist.name")
+    // TODO fix the mess of properties
+    @Mapping(target = "genres", ignore = true)
+    @Mapping(target = "songs", ignore = true)
     AlbumDto toDto(Album entity);
 
+    @Mapping(target = "genres", ignore = true)
+    @Mapping(target = "songs", ignore = true)
     List<AlbumDto> toDtos(List<Album> entities);
 
     // Special mapping method to break circular dependency
     @Named("toDtoWithoutArtist")
     @Mapping(target = "albumArtist", ignore = true)
+    @Mapping(target = "genres", ignore = true)
+    @Mapping(target = "songs", ignore = true)
     AlbumDto toDtoWithoutArtist(Album entity);
 
     // Helper method for collections
@@ -27,13 +35,16 @@ public interface AlbumMapper {
 
     // Standard entity conversion methods
     // TODO fix this, we don't want to ignore artist
-    @Mapping(target = "artist", ignore = true)
+    @Mapping(target = "genres", ignore = true)
+    @Mapping(target = "songs", ignore = true)
     Album toEntity(AlbumDto dto);
 
     List<Album> toEntities(List<AlbumDto> dtos);
 
     @Named("toEntityWithoutArtist")
     @Mapping(target = "artist", ignore = true)
+    @Mapping(target = "genres", ignore = true)
+    @Mapping(target = "songs", ignore = true)
     Album toEntityWithoutArtist(AlbumDto dto);
 
     @Named("toEntitiesWithoutArtist")
